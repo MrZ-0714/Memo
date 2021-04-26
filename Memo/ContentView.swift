9d9ef75eaeb5ba19831dde019e5695294dd9a5e5
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-26 15:14:58 -0700

    Initial Commit

---
+++ Memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by Zimo Zhao on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
