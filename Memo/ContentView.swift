99264f383b0ed06b41b6e5f4c4161548cdf1de2d
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-26 17:01:54 -0700

    Init swift UI with features like HStack, VStack, and it's methods.

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(0..<4) { index in
+                HStack {
+                    ForEach(0..<4)
+                    { index in CardView(isFaceUp: false)
+                    }
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 20.0).stroke(lineWidth: 3.0)
+                Text("🧜‍♀️")
+            } else {
+                RoundedRectangle(cornerRadius: 20.0).fill()
+            }
+
+        }
     }
 }
